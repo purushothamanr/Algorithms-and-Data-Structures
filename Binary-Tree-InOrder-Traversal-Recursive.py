f300b22b9210afacafd2470d3ddacd174d7f6160
Purushothaman Ramalingam <purushoth.kmr@gmail.com>
2018-02-21 19:58:28 +0000

    Binary-Tree-InOrder-Traversal- Recursive Solution

---
+++ Binary-Tree-InOrder-Traversal-Recursive.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        final_list = []
+        
+        self.recursive_solution(root, final_list)
+        
+        return final_list
+    
+    def recursive_solution(self, root, final_list):
+        
+        if not root:
+            return
+        
+        self.recursive_solution(root.left, final_list)
+        
+        final_list.append(root.val)
+        
+        self.recursive_solution(root.right, final_list)
